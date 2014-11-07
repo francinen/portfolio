de2e41891a7f73aeabb9c271cdcbe5e24eb3a486
Francine N <hello@francine.codes>
2014-11-07 17:08:55 -0500

    PUSH IT

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,59 @@
+
+	
+
+
 $(function(){
+
+// Array of elements with class of surprise
+// Array of possible classes
+// Generate random colour
+// Assign class based on random colour
+// Colour can only be used ONCE
+// Run an indefinite number of times
+
+// var assignClass = function(){
+
+// };
+
+
+
+	// var randomNumber = function(n){
+	// 	return Math.floor(Math.random()*n);
+	// }
+	// var randomColour = function(options){
+	// 	return options[randomNumber(options.length)];
+	// }
+
+	// // create a helper tool to shuffle the array
+	// function shuffle(o){ //v1.0
+	//     for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+	//     return o;
+	// };
+
+	// setInterval(function(){
+	// 	var classes = ['green','orange','red','blue'];
+	// 	// shuffl the array and store it in order var
+	// 	var order = shuffle(classes);
+
+	// 	// loop though each suprise and assign it the nth colour
+	// 	$('.surprise').each(function(i,el) {
+	// 		$(this).removeClass('green orange red blue');
+	// 		$(this).addClass(order[i]);
+	// 	});
+
+	// },700);
+
+
+	
+
+	// var y = $(window).scrollTop();
+	// var x = $('.mySkills').position();
+	// if (y < x.top){
+
+
+	// }
+
+
 
 	// $('li.portfolio').on('click',function(){
 	// 	$(this).addClass('open');
